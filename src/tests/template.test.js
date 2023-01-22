531743a082993dfb74032bcfbbcb1bbe657007dd
Paul Lockyer <plockyer@googlemail.com>
2023-01-22 14:32:31 +0000

    Added test template

---
+++ src/tests/template.test.js
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
+
+// Import components we are testing
+
+xdescribe('<Component />', () => {
+
+  xit('renders the component', async () => {
+    
+    // Setup
+
+    // Execute
+    //render(<Component />)
+
+    // Assert
+
+  })
+
+  xit('renders the component and contains some text', async () => {
+    
+    // Setup
+
+    // Execute
+    //render(<Component />)
+
+    // Assert
+    //expect(screen.getByRole('heading')).toHaveTextContent('Hello World! Test Page')
+
+  })
+
+})
