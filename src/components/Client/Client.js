28f217db8f3a5081a6f16e7b36150e55325fc477
Paul Lockyer <plockyer@googlemail.com>
2023-01-23 14:00:51 +0000

    skeleton client component create

---
+++ src/components/Client/Client.js
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+import Layout from '../Layout/Layout'
+
+const Client = () => {
+  return (
+    <Layout>
+      <Routes>
+        
+      </Routes>
+    </Layout>
+  )
+}
+
+export default Client;
