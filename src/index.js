1a2f3c7b55d324b4ae53876a0bd05e98c648f089
Paul Lockyer <plockyer@googlemail.com>
2023-01-23 12:02:36 +0000

    Added routing and client component

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
 /* Packages needed */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 /* Import the apps store */
 import store from './store/store'
 
 /* Components needed */
-import App from './App';
+import Client from './components/Client/Client'
 
 /* Styles needed */
 import './index.css';
 
 const root = document.getElementById('root')
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <Routes>
+
+        {/* Client interface */}
+        <Route path='/*' element={<Client />} />
+        
+
+        {/* Administration interface */}
+        <Route path='/admin/*' element={<><h2>Administration website</h2></>} />
+
+      </Routes>
+    </Provider>
+  </BrowserRouter>,
   root
 );
