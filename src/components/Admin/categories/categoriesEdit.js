2e7bf3b49b7b98e4bb421780fe0b5d7c22dae09a
Paul Lockyer <plockyer@googlemail.com>
2023-03-09 16:45:37 +0000

    added admin resource for categories

---
+++ src/components/Admin/categories/categoriesEdit.js
@@ -0,0 +1,35 @@
+import { 
+    Edit,
+    SimpleForm,
+    DateTimeInput, 
+    EditButton,
+    ShowButton,
+    DeleteButton,
+    TextInput,
+    required,
+    minLength,
+    maxLength,
+    useRecordContext
+} from 'react-admin';
+
+/* Generate the title for the page */
+const PageTitle = () => {
+
+    /* Get the recordContext so we have access to the data displayed in the edit page */
+    const record = useRecordContext()
+    return <span>Post { record ? `"${record.name}"` : '' } </span>
+}
+
+/* Validation functions */
+const validateName = [required(), minLength(2), maxLength(255)]
+
+export const CategoryEdit = () => (
+    <Edit title={<PageTitle />}>
+        <SimpleForm>
+            <TextInput source="id" disabled />
+            <TextInput source="name" validate={validateName} />
+            <DateTimeInput source="created_at" />
+            <DateTimeInput source="updated_at" />
+        </SimpleForm>
+    </Edit>
+);
