ecfef1f210a9cceb7069f5476618d42ec99eda1f
Paul Lockyer <plockyer@googlemail.com>
2023-03-16 22:15:32 +0000

    recipe crud forms added

---
+++ src/components/Admin/recipes/recipeShow.js
@@ -0,0 +1,75 @@
+import { 
+    NumberField, 
+    ReferenceField, 
+    Show,  
+    TextField,
+    TabbedShowLayout,
+    ArrayField,
+    Datagrid,
+    ChipField,
+    SingleFieldList,
+    SimpleShowLayout,
+    List,
+    WrapperField,
+    Labeled
+} from 'react-admin';
+
+/* add custom components for displaying data */
+export const RecipeShow = () => (
+    <Show>
+        <TabbedShowLayout spacing={2}>
+
+            <TabbedShowLayout.Tab label="Summary">
+
+                <TextField source="id" />
+                <ArrayField source="categories">
+                    <SingleFieldList>
+                        <ChipField source="name" />
+                    </SingleFieldList>
+                </ArrayField>
+                <ReferenceField source="userId" reference="users" >
+                    <TextField source="email" />
+                </ReferenceField>
+                <TextField source="name" />
+                <TextField source="description" />
+                <NumberField source="servings" />
+                <NumberField source="calories_per_serving" />
+                <NumberField source="prep_time" />
+                <NumberField source="cook_time" />
+                <TextField source="rating" />
+                
+            </TabbedShowLayout.Tab>
+
+            <TabbedShowLayout.Tab label="Ingredients">
+
+                <ArrayField source="ingredients">
+                    <Datagrid bulkActionButtons={false} empty={<div>No ingredients found</div>}>
+                        <TextField source="name" />
+                        <NumberField source="amount" />
+                        <TextField source="amount_type" />
+                    </Datagrid>
+                </ArrayField>
+            </TabbedShowLayout.Tab>
+
+            <TabbedShowLayout.Tab label="Steps">
+                <ArrayField source="steps">
+                    <Datagrid bulkActionButtons={false} empty={<div>No steps found</div>}>
+                        <TextField source="stepNo" />
+                        <TextField source="content" multiline />
+                    </Datagrid>
+                </ArrayField>
+            </TabbedShowLayout.Tab>
+
+            <TabbedShowLayout.Tab label="Cookbooks">
+                <ArrayField source="cookbooks">
+                    <Datagrid bulkActionButtons={false} empty={<div>No cookbooks found</div>}>
+                        <TextField source="name" />
+                        <TextField source="description" multiline />
+                        <TextField source="image" />
+                    </Datagrid>
+                </ArrayField>
+            </TabbedShowLayout.Tab>
+
+        </TabbedShowLayout>
+    </Show>
+);
