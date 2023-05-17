7236da24dc662631bd0dfcdba9428ef2ada6e1bc
Paul Lockyer <plockyer@googlemail.com>
2023-05-17 14:36:43 +0100

    made into a card and allowed more than image to be shown

--- src/components/Admin/CustomUI/ShowImage.js
+++ src/components/Admin/CustomUI/ShowImage.js
@@ -3,12 +3,47 @@ import {
 } from 'react-admin'
 
 import './ShowImage.css'
+import ShowImageTitle from './ShowImageTitle'
 
 const ShowImage = () => {
     const record = useRecordContext()
+    let images = []
+    
+    if(record?.pictures && record?.pictures.length >= 1){
+        record.pictures.forEach(picture => {
+                images.push({
+                    src: picture.src,
+                    title: picture.title
+                })
+            })
+    } else if (record?.pictures){
+        images.push({ 
+            src: record.pictures.src,
+            title: record.pictures.title
+        })
+    } else if (record?.src) {
+        images.push({ 
+            src: record.src,
+            title: record.title
+        })
+    } 
+
+    console.log('Final images list: ', images)
+
     return (
         <span className="imageContainer">
-            <img src={record.src} alt={record.title} className="preview" />
+
+            <div className="cards">
+            {images.length > 0 && images.map(image => (
+                    <div className="card">
+                        <img src={image.src} title={image.title} alt={image.title} />
+                        <div className="card-info">
+                            <h3>{image.title}</h3>
+                        </div>
+                    </div>
+            ))}
+            </div>
+
         </span>
     )
 }
