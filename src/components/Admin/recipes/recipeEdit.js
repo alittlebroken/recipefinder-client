d526d34eed31416a287e37e3aadb7702c3ba40a2
Paul Lockyer <plockyer@googlemail.com>
2023-05-25 16:54:33 +0100

    modified to easily add, update or delete images

--- src/components/Admin/recipes/recipeEdit.js
+++ src/components/Admin/recipes/recipeEdit.js
@@ -14,7 +14,18 @@ import {
     SelectInput,
     SimpleFormIterator,
     ArrayInput,
+    ArrayField,
+    TextField,
+    Datagrid,
+    EditButton,
+    DeleteButton,
+    ImageInput,
+    ImageField
 } from 'react-admin';
+
+/* Import custom components */
+import ShowImage from '../CustomUI/ShowImage'
+import { ShowImageTitle } from '../CustomUI/ShowImageTitle'
 
 /* Generate the title for the page */
 const PageTitle = () => {
@@ -23,6 +34,23 @@ const PageTitle = () => {
     const record = useRecordContext()
     return <span>Edit { record ? `"${record.name}"` : '' } </span>
 }
+
+/* Get the ENV we are for the file upload name */
+const getFileUploadName = () => {
+    return process.env.REACT_APP_ENV === 'production' ? 'images' : 'tests'
+}
+
+/* Display an image */
+const DataGridImage = () => {
+    const record = useRecordContext()
+    const divStyle = {
+        width: '75px',
+        height: '75px',
+        borderRadius: '10px'
+    }
+    return <span><img src={record.src} alt={record.title} style={divStyle} /></span>
+}
+
 
 /* Validation for the inout fields */
 const validateName = [required(), minLength(2), maxLength(255)]
@@ -44,7 +72,10 @@ const validateCookbook = [required()]
 
 const validateCategory = [required()]
 
+const validateImageTitle = [required(), minLength(2)]
+
 export const RecipeEdit = () => (
+
     <Edit title={<PageTitle />}>
 
         <TabbedForm spacing={2}>
@@ -109,6 +140,7 @@ export const RecipeEdit = () => (
             <TabbedForm.Tab label="Categories">
                 
                 <ArrayInput source="categories">
+
                     <SimpleFormIterator inline fullWidth>
                         <ReferenceInput source="categoryId" reference="categories">
                             <SelectInput 
@@ -121,6 +153,25 @@ export const RecipeEdit = () => (
                         </ReferenceInput>
                     </SimpleFormIterator>
                 </ArrayInput>
+
+            </TabbedForm.Tab>
+
+            <TabbedForm.Tab label="Images">
+            
+                    <ImageInput source={getFileUploadName()} label="Images" accept="image/*">
+                        <ImageField source="src" label="title"/>  
+                    </ImageInput>
+                    <TextInput label="Title" source="title" fullWidth validate={validateImageTitle}/>
+
+                <ArrayField source="pictures" fullWidth>
+
+                   <Datagrid bulkActionButtons={false} fullWidth>
+                        <DataGridImage />
+                        <TextField source="title" label="Title" />
+                        <EditButton resource="uploads" />
+                        <DeleteButton resource="uploads" />
+                   </Datagrid>
+                </ArrayField>
 
             </TabbedForm.Tab>
 
