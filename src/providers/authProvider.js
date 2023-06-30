731517bb5236bc81c07e535560c3f75f0206f7dc
Paul Lockyer <plockyer@googlemail.com>
2023-06-30 11:03:05 +0100

    added imports and setup for JWT token processing

--- src/providers/authProvider.js
+++ src/providers/authProvider.js
@@ -1,8 +1,27 @@
 import axios from 'axios'
+import jwt_decode from 'jwt-decode'
 
 import inMemoryJWT from '../utils/auth.utils';
 
-// Wrapper for the methods
+/* Set the base URL for the axios requests */
+const BASEURL = process.env.REACT_APP_API_URL
+
+/* Set the options to use for the axios request */
+const axiosOptions = {
+    withCredentials: true,
+    headers: {
+        'token': inMemoryJWT.getToken(),
+        'Content-type': 'application/json'
+    }
+}
+
+/* Set the refresh rate for refresh tokens */
+const refreshRate = process.env.REACT_APP_TOKEN_REFRESH_RATE || 300
+
+/* set the endpoint for getting refresh tokens */
+inMemoryJWT.setRefreshTokenEndpoint(`${BASEURL}/auth/refresh-token`)
+
+/* Wrapper for the methods */
 const authProvider = {
 
 }
