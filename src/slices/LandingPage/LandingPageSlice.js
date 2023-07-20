b83519906d6378c300824c0056eec319f5d8c114
Paul Lockyer <plockyer@googlemail.com>
2023-07-20 16:31:25 +0100

    added latestRecipes

--- src/slices/LandingPage/LandingPageSlice.js
+++ src/slices/LandingPage/LandingPageSlice.js
@@ -1,8 +1,34 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-import { dataProvider } from '../../providers/dataProvider';
+import { apiProvider } from '../../providers/apiProvider';
 
 // Thunks for gathering the API data
+export const getLatestRecipes = createAsyncThunk('landingpage/getLatestRecipes',
+    async (payload, thunkAPI) => {
+
+        try{
+
+            // Extract the parameters from the payload
+            const resource = payload.resource
+            const params = {
+                sort: {
+                    field: 'created_at',
+                    order: 'desc'
+                },
+                pagination: {
+                    page: 1,
+                    perPage: 5
+                }
+            }
+
+            return await apiProvider.getList(resource, params);
+
+        } catch(error) {
+            throw error;
+        }
+
+    }
+)
 
 // Initial State of this slice
 const initialState = {
@@ -21,11 +47,31 @@ export const landingpageSlice = createSlice({
 
     },
     extraReducers: {
+        [getLatestRecipes.pending]: (state, action) => {
+            state.isLoading = true
+            state.hasError = false
+        },
+        [getLatestRecipes.rejected]: (state, action) => {
+            state.isLoading = false
+            state.hasError = true
+        },
+        [getLatestRecipes.fulfilled]: (state, action) => {
+            state.isLoading = false
+            state.hasError = false
 
+            // Parse the payload
+            const results = JSON.parse(action.payload)
+
+            // Store the results returned
+            state.latest = results?.data
+        }
     },
 })
 
 // Export selectors
+export const selectLatestRecipes = state => state.landingpage?.latest
+export const selectisLoading = state => state.landingpage?.isLoading
+export const selectHasError = state => state.landingPage?.hasError
 
 // Export actions
 
