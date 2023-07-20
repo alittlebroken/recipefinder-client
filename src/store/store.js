4534ce18ee77d0f66c9d6e2af418ab5a0d7d13c8
Paul Lockyer <plockyer@googlemail.com>
2023-07-20 17:39:53 +0100

    updated to add landing page reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 // Import any needed packages
 import { configureStore } from '@reduxjs/toolkit'
 
+import landingReducer from '../slices/LandingPage/LandingPageSlice'
+
 // create the store so we can add slixes to its reducer
 const store = configureStore({
     reducer: {
-
+        landingPage: landingReducer,
     }
 })
 
