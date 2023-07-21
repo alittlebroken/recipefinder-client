ed6072a930bba89005fc61c442461158352749d8
Paul Lockyer <plockyer@googlemail.com>
2023-07-21 10:12:54 +0100

    refactored creation of a store into seperate function

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,23 @@
 // Import any needed packages
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 
 import landingReducer from '../slices/LandingPage/LandingPageSlice'
 
-// create the store so we can add slixes to its reducer
-const store = configureStore({
-    reducer: {
-        landingPage: landingReducer,
-    }
+// Create a root reducer that comines all the slices reducers together
+const rootReducer = combineReducers({
+    landingPage: landingReducer
 })
+
+// Creates a store with all the correct reducers and actions
+const setupStore = preloadedState => {
+    return configureStore({
+        reducer: rootReducer,
+        preloadedState
+    })
+}
+
+// create the store so we can add slixes to its reducer
+const store = setupStore({})
 
 // Export the store
 export default store;
