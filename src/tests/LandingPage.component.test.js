43262116dd44c857cbef5dc974ca749386fcd9fd
Paul Lockyer <plockyer@googlemail.com>
2023-07-21 12:55:45 +0100

    store to be created beforfe each test

--- src/tests/LandingPage.component.test.js
+++ src/tests/LandingPage.component.test.js
@@ -3,6 +3,7 @@ import { screen } from '@testing-library/react'
 
 // Import custom render function
 import { renderWithProviders } from '../utils/test.utils'
+import { setupStore } from '../store/store'
 
 // Import Components being tested
 import LandingPage from '../components/Client/LandingPage/LandingPage'
@@ -18,11 +19,19 @@ const initialStoreState = {
     }
 }
 
+// setup a store for the tests to use
+let store
+
 describe('LandingPage Component', () => {
+
+    beforeEach(() => {
+        // Create an initial store
+        store = setupStore(initialStoreState)
+    })
 
     it('', async () => {
 
-        // Setup
+        // 
         
 
     })
