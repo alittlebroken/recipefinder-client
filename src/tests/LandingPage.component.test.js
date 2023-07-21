3c7f5f312697df575357431f27863397b0fb076a
Paul Lockyer <plockyer@googlemail.com>
2023-07-21 10:39:23 +0100

    skeleton for tests created

---
+++ src/tests/LandingPage.component.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { screen } from '@testing-library/react'
+
+// Import custom render function
+import { renderWithProviders } from '../utils/test.utils'
+
+// Import Components being tested
+import LandingPage from '../components/Client/LandingPage/LandingPage'
+
+describe('LandingPage Component', () => {
+
+    it('', async () => {
+        
+    })
+
+})
