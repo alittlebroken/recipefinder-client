19d5f82aabc09f478ff490463a178560324c1830
Paul Lockyer <plockyer@googlemail.com>
2023-07-21 12:49:59 +0100

    initial store ttate added to test globals

--- src/tests/LandingPage.component.test.js
+++ src/tests/LandingPage.component.test.js
@@ -7,10 +7,24 @@ import { renderWithProviders } from '../utils/test.utils'
 // Import Components being tested
 import LandingPage from '../components/Client/LandingPage/LandingPage'
 
+// Inital store state
+const initialStoreState = {
+    landingpage: {
+        latest: [],
+        popular: [],
+        categories: [],
+        isLoading: false,
+        hasError: false,
+    }
+}
+
 describe('LandingPage Component', () => {
 
     it('', async () => {
+
+        // Setup
         
+
     })
 
 })
