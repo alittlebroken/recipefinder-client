3fab86f3720060e31d9438574887f9d59b824a87
Paul Lockyer <plockyer@googlemail.com>
2023-07-25 16:55:07 +0100

    styles to add via props added to Card

--- src/components/UI/Cards/Card.js
+++ src/components/UI/Cards/Card.js
@@ -1,15 +1,41 @@
 import './Card.css'
 
-const Card = ({children}) => {
+/* Card base */
+const Card = (props) => {
+
+    // Destructure the props
+    const { 
+        children,
+        rounded,
+        shadow,
+        thinBorder,
+        mediumBorder,
+        thickBorder,
+        otherClasses,
+    } = props
+
+    // Apply various classes based on the props passed in
+    let className = 'card'
+
+    className += rounded ? ' card-rounded' : null
+    className += shadow ? ' card-shadow' : null
+    className += thinBorder ? ' card-border-thin' : null
+    className += mediumBorder ? ' card-border-medium' : null
+    className += thickBorder ? ' card-border-thick' : null
+    
+    // Finally other styles here - Should be a string of class names starting with a space
+    className += otherClasses
 
     return (
         <>
-            <div aria-label="card-container" className="card w-100">
+            <div aria-label="card-container" className={className}>
                 {children}
             </div>
         </>
     )
 
 }
+
+
 
 export default Card;
