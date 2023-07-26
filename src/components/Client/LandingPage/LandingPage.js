c3869274d5ea7e361880b45874f97506025b94dd
Paul Lockyer <plockyer@googlemail.com>
2023-07-26 09:21:35 +0100

    initial skeleton created

--- src/components/Client/LandingPage/LandingPage.js
+++ src/components/Client/LandingPage/LandingPage.js
@@ -1,1 +1,15 @@
 import "./LandingPage.css"
+
+import LatestRecipes from '../LatestRecipes/LatestRecipes'
+
+const LandingPage = (props) => {
+
+
+    return (
+
+       <LatestRecipes /> 
+
+    )
+}
+
+export default LandingPage
