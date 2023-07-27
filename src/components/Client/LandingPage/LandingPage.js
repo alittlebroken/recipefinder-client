0d951dc5422d0aac0856fa2a93e60e0e116482df
Paul Lockyer <plockyer@googlemail.com>
2023-07-27 16:56:34 +0100

    fetch latest recipes from store

--- src/components/Client/LandingPage/LandingPage.js
+++ src/components/Client/LandingPage/LandingPage.js
@@ -1,13 +1,36 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import "./LandingPage.css"
 
 import LatestRecipes from '../LatestRecipes/LatestRecipes'
 
+import {
+    getLatestRecipes,
+    selectLatestRecipes,
+    selectisLoading,
+    selectHasError
+} from '../../../slices/LandingPage/LandingPageSlice'
+
 const LandingPage = (props) => {
 
+    // Alias the dispatch hook
+    const dispatch = useDispatch()
+
+    // Get the data we need for the component
+    useEffect(() => {
+
+        dispatch(getLatestRecipes())
+
+    },[]) // Only perform when the component mounts 
+
+    // Get the data for the various components
+    const latestRecipeData = useSelector(selectLatestRecipes)
+    console.log(latestRecipeData)
 
     return (
         <div aria-label="Landing page for website" className="landing-container">
-            <LatestRecipes /> 
+            <LatestRecipes recipes={latestRecipeData}/> 
         </div>
     )
 }
