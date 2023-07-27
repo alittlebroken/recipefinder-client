8260135de1869ff6ad25b69812484fd422baa4d4
Paul Lockyer <plockyer@googlemail.com>
2023-07-27 10:41:04 +0100

    added button component and modified click hander

--- src/components/Client/Recipe/Recipe.js
+++ src/components/Client/Recipe/Recipe.js
@@ -1,5 +1,6 @@
 /* Import other components */
 import Card from '../../UI/Cards/Card'
+import Button from  '../../UI/Button/Button'
 import { useNavigate } from 'react-router-dom'
 
 const Recipe = (props) => {
@@ -9,23 +10,29 @@ const Recipe = (props) => {
 
     /* Handle a click to show more details for a recipe */
     const navigate = useNavigate()
-    const handleClick = (id) => {
-        navigate(`/recipe/${id}`)
-    }
 
     return (
-        <Card>
+        <Card 
+         
+        thinBorder 
+        >
             <Card.Image 
                 source={record.src} 
                 title={record.title}
                 altText={record.alt}
+                
             /> 
             <Card.Body>
                 <Card.Title text={record.name} medium />
                 {record.textContent}
             </Card.Body>
             <Card.Actions>
-                <button onClick={handleClick(record.id)}>More</button>
+                <Button 
+                clickHandler={() => {navigate(`/recipe/${record.id}`)}} 
+                
+                >
+                    More
+                </Button>
             </Card.Actions>
         </Card>
     )
