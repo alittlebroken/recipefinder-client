38dc0ba8c46ba7bdb8b58e2d05c28746dd7b5bcf
Paul Lockyer <plockyer@googlemail.com>
2023-07-31 15:08:21 +0100

    added PopularRecipes component

--- src/components/Client/LandingPage/LandingPage.js
+++ src/components/Client/LandingPage/LandingPage.js
@@ -4,11 +4,14 @@ import { useDispatch, useSelector, shallowEqual  } from 'react-redux';
 import "./LandingPage.css"
 
 import LatestRecipes from '../LatestRecipes/LatestRecipes'
+import PopularRecipes from '../PopularRecipes/PopularRecipes'
 
 import {
     getLatestRecipes,
     selectLatestCount,
     selectLatestRecipes,
+    getPopularRecipes,
+    selectPopularRecipes,
     selectisLoading,
     selectHasError
 } from '../../../slices/LandingPage/LandingPageSlice'
@@ -21,18 +24,21 @@ const LandingPage = (props) => {
     // Get the data for the various components
     let latestRecipeData = useSelector(selectLatestRecipes)
     let latestCount = useSelector(selectLatestCount)
+    let popularRecipeData = useSelector(selectPopularRecipes)
     let loading = useSelector(selectisLoading)
 
     // Get the data we need for the component
     useEffect(() => {
 
         dispatch(getLatestRecipes())
+        dispatch(getPopularRecipes())
 
     }, [dispatch]) // Only perform when the component mounts 
 
     return (
         <div aria-label="Landing page for website" className="landing-container">
            { loading ? 'Loading latest recipe data' : (<LatestRecipes recipes={latestRecipeData}/>) }
+           { loading ? 'Loading popular recipe data' : (<PopularRecipes records={popularRecipeData}/>)}
         </div>
     )
 }
