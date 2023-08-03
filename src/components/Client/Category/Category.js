c69153ab9dad6fb5732af5486bcd450edcaeaa5b
Paul Lockyer <plockyer@googlemail.com>
2023-08-03 14:53:49 +0100

    turned Card container into a link

--- src/components/Client/Category/Category.js
+++ src/components/Client/Category/Category.js
@@ -11,17 +11,19 @@ const Category = (props) => {
 
     return (
         <Card key={record.id} overlay rounded>
-            <Card.Container>
-                <Card.Image 
-                 rounded 
-                 source={"https://media.istockphoto.com/id/1278745986/photo/culinary-background-with-traditional-ingredients-of-italian-cuisine.webp?b=1&s=170667a&w=0&k=20&c=jct5dpWc9sFrs6g4_u9n1aV7IDBEJ5n3T9lF75sMft8="}
-                 alt="Various veg on a slate board with some oil" 
-                 title="Various veg on a slate board with some oil"
-                />
-                <Card.Overlay center>
-                    <Card.Title text={record.name} medium/>
-                </Card.Overlay>
-            </Card.Container>
+            <a href={`/recipes?cat=${record.id}`}>
+                <Card.Container>
+                    <Card.Image 
+                    rounded 
+                    source={"https://media.istockphoto.com/id/1278745986/photo/culinary-background-with-traditional-ingredients-of-italian-cuisine.webp?b=1&s=170667a&w=0&k=20&c=jct5dpWc9sFrs6g4_u9n1aV7IDBEJ5n3T9lF75sMft8="}
+                    alt="Various veg on a slate board with some oil" 
+                    title="Various veg on a slate board with some oil"
+                    />
+                    <Card.Overlay center>
+                        <Card.Title text={record.name} medium />
+                    </Card.Overlay>
+                </Card.Container>
+            </a>
         </Card>
     )
 }
