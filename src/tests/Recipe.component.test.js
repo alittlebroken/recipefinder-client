37ef9f7c03a61ff825f61678ad409c1a6b321228
Paul Lockyer <plockyer@googlemail.com>
2023-08-04 15:18:37 +0100

    refactored tests

--- src/tests/Recipe.component.test.js
+++ src/tests/Recipe.component.test.js
@@ -7,6 +7,28 @@ import { render, screen, within } from '@testing-library/react'
 /* Import in the component being tested */
 import Recipe from '../components/Client/Recipe/Recipe'
 
+/* Mock the component props */
+const mockedRecord = {
+    id: 1,
+    name: 'Sausages and Mash',
+    images: [{
+        imageId: 36,
+        source: '/public/test.png',
+        title: 'Plate with sausages, mash and herbs',
+        alt: 'Plate with sausages, mash and herbs'
+    }],
+    categories: [
+        {
+            id: 67,
+            name: 'Lunch'
+        },
+        {
+            id: 68,
+            name: 'Dairy Free'
+        }
+    ]
+}
+
 describe('Recipe component', () => {
 
     afterEach(() => {
@@ -18,22 +40,24 @@ describe('Recipe component', () => {
         /* Setup */
 
         /* Execute */
-        render(<Recipe />)
+        render(<Recipe record={mockedRecord} />)
 
         /* Test */
         expect(screen).toBeDefined()
 
         const children = screen.getByRole('generic', { name: /card-container/i })
-
-        expect(within(children).getAllByRole('heading')).toHaveLength(1)
-        expect(within(children).getAllByRole('img')).toHaveLength(1)
-        expect(within(children).getAllByRole('generic', { name: /rating-star /i})).toHaveLength(5)
-        expect(within(children).getAllByRole('button')).toHaveLength(1)
-        expect(within(children).getAllByRole('paragraph')).toHaveLength(1)
+        expect(within(children).getByText('Sausages and Mash')).toBeInTheDocument()
         
+        expect(within(children).getAllByRole('link')).toHaveLength(1)
+        expect(within(children).getByRole('link')).toHaveAttribute('href','/recipe/1')
+        
+        expect(within(children).getAllByRole('img')).toHaveLength(1)
 
+        const cats = screen.getByRole('generic', { name: /card tag list/i })
+        expect(within(cats).getAllByRole('generic', { name: /card tag/i })).toHaveLength(2)
+        expect(within(cats).getByText('Lunch')).toBeInTheDocument()
+        expect(within(cats).getByText('Dairy Free')).toBeInTheDocument()
+        
     })
-
-
 
 })
