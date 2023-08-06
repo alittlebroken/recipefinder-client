90a6e4c647790a9fd68513519e3b285be3fa7257
Paul Lockyer <plockyer@googlemail.com>
2023-08-06 21:42:28 +0100

    refactored tests

--- src/tests/LandingHero.component.test.js
+++ src/tests/LandingHero.component.test.js
@@ -3,64 +3,32 @@ import { screen, within  } from '@testing-library/react'
 
 // Import custom render function
 import { renderWithProviders } from '../utils/test.utils'
-import { setupStore } from '../store/store'
 
 // Import components being tested
 import LandingHero from '../components/Client/LandingHero/LandingHero'
-import Hero from '../components/Client/Hero/Hero'
-
-// Inital store state
-const initialStoreState = {
-    landingpage: {
-        latest: [],
-        popular: [],
-        categories: [],
-        isLoading: false,
-        hasError: false,
-    }
-}
-
-// setup a store for the tests to use
-let store
-
-// Mock any child components
-jest.mock('../components/Client/Hero/Hero')
 
 describe('LandingCategories component', () => {
-
-    beforeEach(() => {
-        // Create an initial store
-        store = setupStore(initialStoreState)
-    })
 
     it("renders the component", async () => {
 
         // render the component
-        renderWithProviders(<LandingHero />, { store })
+        renderWithProviders(<LandingHero />)
 
         // Asserts
         expect(screen).toBeDefined()
-        expect(screen.getByRole('heading', { name: /Find your next favourite/i })).toBeInTheDoucument()
 
-    })
+        expect(screen.getByRole('heading', { name: /Find your favourite/i })).toBeInTheDocument()
 
-    it("The child component is called", async () => {
+        expect(screen.getByRole('button', { name: /GO/i })).toBeInTheDocument()
 
-        // render the component
-        renderWithProviders(<LandingHero />, { store })
-
-        // Asserts
-        expect(screen).toBeDefined()
-        expect(screen.getByRole('heading', { name: /Find your next favourite/i })).toBeInTheDoucument()
-
-        expect(Hero).toHaveBeenCalled()
+        expect(screen.getByRole('searchbox', { placeholder: /Type in a recipe name or ingredient/i })).toBeInTheDocument()
 
     })
 
     xit("Skeleton test", async () => {
 
         // render the component
-        renderWithProviders(<LandingHero />, { store })
+        renderWithProviders(<LandingHero />,)
         
         // Setup
         const latestRecipes = screen.getByRole('generic', { name: /recipes-Container/i })
