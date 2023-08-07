8bff294ec37ae7d26c1aa1ebe8e302978d708102
Paul Lockyer <plockyer@googlemail.com>
2023-08-07 16:06:38 +0100

    refactored tests

--- src/tests/LandingPage.component.test.js
+++ src/tests/LandingPage.component.test.js
@@ -1,58 +1,35 @@
 import React from 'react'
-import { screen } from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
+
+import LandingPage from '../components/Client/LandingPage/LandingPage'
 
 // Import custom render function
 import { renderWithProviders } from '../utils/test.utils'
-import { setupStore } from '../store/store'
-
-// Import Components being tested
-import LandingPage from '../components/Client/LandingPage/LandingPage'
-import LatestRecipes from '../components/Client/LatestRecipes'
-import PopularRecipes from '../components/Client/PopularRecipes'
-import Categories from '../components/Client/Categories'
-
-// Inital store state
-const initialStoreState = {
-    landingpage: {
-        latest: [],
-        popular: [],
-        categories: [],
-        isLoading: false,
-        hasError: false,
-    }
-}
-
-// setup a store for the tests to use
-let store
-
-// Mock the child components
-jest.mock('../components/Client/LatestRecipes/LatestRecipes')
-jest.mock('../components/Client/PopularRecipes/PopularRecipes')
-jest.mock('../components/Client/Categories/Categories')
 
 describe('LandingPage Component', () => {
-
-    beforeEach(() => {
-        // Create an initial store
-        store = setupStore(initialStoreState)
-    })
 
     it('the component renders', async () => {
 
         // render the component
-        renderWithProviders(<LandingPage />, { store })
-        
-        // Asserts
-        expect(LatestRecipes).toHaveBeenCalled()
-        expect(PopularRecipes).toHaveBeenCalled()
-        expect(Categories).toHaveBeenCalled()
+        renderWithProviders(<LandingPage />)
+    
+        // Render loading data message for each Component
+        expect(screen.getByText(/Loading latest recipe data/i)).toBeInTheDocument()
+        expect(screen.getByText(/Loading popular recipe data/i)).toBeInTheDocument()
+        expect(screen.getByText(/Loading category data/i)).toBeInTheDocument()
+
+        let headings
+        await waitFor(() => {
+            headings = screen.getAllByRole('heading')
+            expect(headings).toHaveLength(4)
+        })
 
     })
 
     xit('Skeleton test', async () => {
 
         // render the component
-        renderWithProviders(<LandingPage />, { store })
+        renderWithProviders(<LandingPage />)
         
         // Asserts
         expect(screen.getByRole('generic', { name: /landing-container/i })).toBeInTheDocument()
