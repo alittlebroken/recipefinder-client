f9162a23bb744af195e00776ce9b2cfd38ad78a8
Paul Lockyer <plockyer@googlemail.com>
2023-08-10 20:41:04 +0100

    added getting options from the Search slice store

--- src/components/UI/Select/Select.js
+++ src/components/UI/Select/Select.js
@@ -1,6 +1,15 @@
 import './Select.css'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { 
+    selectSearchOptions,
+    setSearchOptions 
+} from '../../../slices/Search/SearchSlice'
 
 const Select  = (props) => {
+
+    /* Alias the dispatcher */
+    const dispatch = useDispatch()
 
     /* Destructure the passed in props */
     const { 
@@ -8,13 +17,24 @@ const Select  = (props) => {
         name, 
         is, 
         id, 
-        selected } = props
+        selected
+    } = props
 
     /* Set up the clkasses to be used for the component */
     let classes = 'search-select'
 
+    /* Get any data that we need from the store */
+    const searchOptions = useSelector(selectSearchOptions)
+
+    /* Handlers for interactivity for the component*/
+    const handleChange = (e) => {
+        e.preventDefault()
+        /* Set the new search Option */
+        dispatch(setSearchOptions(e.target.value))
+    }
+
     return (
-        <select name={name} style={is} id={id} className={classes}>
+        <select name={name} style={is} id={id} className={classes} onChange={handleChange}>
             {options.map((option) => {
                 if(selected === option.value){
                     return <option value={option.value} selected>{option.value}</option>
