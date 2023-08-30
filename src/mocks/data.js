ec6e5f121f28b010264f48e446f606dd5451f651
Paul Lockyer <plockyer@googlemail.com>
2023-08-30 21:28:12 +0100

    data for the mocks to use

---
+++ src/mocks/data.js
@@ -0,0 +1,17 @@
+/* List of users for the tests */
+export const users = [
+    {
+        username: 'customer@website.com',
+        password: 'Cust0mBuy3r'
+    },
+    {
+        username: 'admin@webiste.com',
+        password: 'Adm!n1str4t0r'
+    }
+]
+
+/* List of tokens returned by the Auth system */
+export const tokens = {
+  accessToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+  refreshToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'  
+}
