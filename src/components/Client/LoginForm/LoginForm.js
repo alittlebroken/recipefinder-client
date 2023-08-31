9f667cf85c0c6896d892ee1981d9cf6a37478df5
Paul Lockyer <plockyer@googlemail.com>
2023-08-31 21:14:12 +0100

    added redirect on login and set the access token state

--- src/components/Client/LoginForm/LoginForm.js
+++ src/components/Client/LoginForm/LoginForm.js
@@ -1,10 +1,17 @@
 import { useState, useContext } from "react"
-import { redirect } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
+import { nanoid } from "@reduxjs/toolkit"
 
 /* Import provider context  */
-import { ProviderContext } from '../../../contexts/providers'
+import { ProviderContext, AccessTokenProvider, useAccessToken } from '../../../contexts/providers'
 
 const LoginForm = () => {
+
+    /* Navigation to redirect the login form */
+    const navigate = useNavigate()
+
+    /* Set the access token state */
+    const [accessToken, setAccessToken] = useAccessToken()
 
     /* contexts to be used by the component */
     const providers = useContext(ProviderContext)
@@ -45,6 +52,15 @@ const LoginForm = () => {
 
     }
 
+    /* Handlers for updating the form inputs */
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value)
+    }
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value)
+    }
+
     /* Handle submission of the form */
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -52,7 +68,7 @@ const LoginForm = () => {
         /* Validate the form inputs */
         handleValidation()
 
-        /* Perform the login */
+        /* Perform the login 
         const result = providers.authProvider.login(username, password)
         if(result !== true){
             setErrors([
@@ -60,9 +76,16 @@ const LoginForm = () => {
                 result.message
             ])
         }
-
-        /* redirect to the main page */
-        redirect("/")
+        */
+        providers.authProvider.login(username, password)
+        .then((result) => {
+            setAccessToken(result)
+            /* redirect to the main page */
+            return navigate("/")
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 
     }
 
@@ -70,12 +93,12 @@ const LoginForm = () => {
     return (
         <form onSubmit={handleSubmit} className="flex flex-col">
             <label htmlFor="email">Email address:</label>
-            <input type="text" id="email" placeholder="Email address" />
+            <input type="text" id="email" placeholder="Email address" onChange={handleUsernameChange} />
             <label htmlFor="password">Password:</label>
-            <input type="password" id="password" />
+            <input type="password" id="password"  onChange={handlePasswordChange} />
             <button type="submit">Submit</button>
             {errors.map((error) => {
-                return (<p className="formError">{error}</p>)
+                return (<p key={nanoid()} className="formError">{error}</p>)
             })}
             <hr />
             <a href="/signup">Sign Up</a>
