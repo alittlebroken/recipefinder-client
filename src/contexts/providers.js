114e6e58035558984e4435fe0067f0c64db1d29a
Paul Lockyer <plockyer@googlemail.com>
2023-08-31 20:38:08 +0100

    updated to have a specific context for ther accessToken

--- src/contexts/providers.js
+++ src/contexts/providers.js
@@ -1,3 +1,14 @@
-import { createContext } from "react"
+import { createContext, useContext, useState } from "react"
+
+/* Create a Provider to store the access tokens in memory */
+const AccessToken = createContext(null)
+
+const AccessTokenProvider = (props) => {
+    const [accessToken, setAccessToken] = useState(null)
+    return <AccessToken.Provider value={[accessToken, setAccessToken]} {...props} />
+}
+
+const useAccessToken = () => useContext(AccessToken)
+export { AccessTokenProvider, useAccessToken }
 
 export const ProviderContext = createContext()
