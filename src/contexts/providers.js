2115df4f06d2de7078c97a2f4209632bb9cd31ba
Paul Lockyer <plockyer@googlemail.com>
2023-09-01 09:29:11 +0100

    changed order of var creation

--- src/contexts/providers.js
+++ src/contexts/providers.js
@@ -1,12 +1,12 @@
 import { createContext, useContext, useState } from "react"
 
-/* Create a Provider to store the access tokens in memory */
-const AccessToken = createContext(null)
-
 const AccessTokenProvider = (props) => {
     const [accessToken, setAccessToken] = useState(null)
     return <AccessToken.Provider value={[accessToken, setAccessToken]} {...props} />
 }
+
+/* Create a Provider to store the access tokens in memory */
+const AccessToken = createContext(null)
 
 const useAccessToken = () => useContext(AccessToken)
 export { AccessTokenProvider, useAccessToken }
