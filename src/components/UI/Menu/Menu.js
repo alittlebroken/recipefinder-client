93ad2adf6494af538bf690b47e9d9fc2f9ecae03
Paul Lockyer <plockyer@googlemail.com>
2023-09-02 08:29:58 +0100

    Initial creation

---
+++ src/components/UI/Menu/Menu.js
@@ -0,0 +1,18 @@
+import './Menu.css'
+
+const Menu = (props) => {
+
+    /* Destructure the passed in props */
+    const { 
+        title
+    } = props
+
+    return (
+        <>
+        
+        </>
+    )
+
+}
+
+export default Menu
