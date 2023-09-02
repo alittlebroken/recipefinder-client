bccc656f23f7f6b5bf74aca5b35ba9b7fe6dccce
Paul Lockyer <plockyer@googlemail.com>
2023-09-02 08:18:35 +0100

    removed old validation and added new as well as updated signup text

--- src/components/Client/LoginForm/LoginForm.js
+++ src/components/Client/LoginForm/LoginForm.js
@@ -21,68 +21,42 @@ const LoginForm = () => {
 
     /* Setup state for handling the form inputs */
     const [username, setUsername] = useState('')
+    const [usernameErrors, setUsernameErrors] = useState('')
     const [password, setPassword] = useState('')
-
-    /* State for any error messages */
-    const [errors, setErrors] = useState([])
-
-    /* Handle validation of the form */
-    const handleValidation = () => {
-
-        /* Set errors */
-        const validationErrors = []
-
-        /* Check if username and password are missing */
-        if((!username || username === undefined) && (!password || password === undefined)){
-            validationErrors.push('You must supply username and password')
-            setErrors(validationErrors)
-            return
-        }
-
-        /* Is the username set */
-        if(!username || username === undefined){
-            validationErrors.push('You must supply a username')
-            setErrors(validationErrors)
-            return
-        }
-
-        /* Is the password set */
-        if(!password || password === undefined){
-            validationErrors.push('You must supply a password')
-            setErrors(validationErrors)
-            return
-        }
-
-    }
 
     /* Handlers for updating the form inputs */
     const handleUsernameChange = (e) => {
         setUsername(e.target.value)
+        if(e.target.value.length === 0){
+            setUsernameErrors('You must specify a username')
+        } else if (e.target.value.length < 8){
+            setUsernameErrors('Username must be 8 characters or longer')
+        }
     }
 
     const handlePasswordChange = (e) => {
         setPassword(e.target.value)
+        
     }
 
     /* Handle submission of the form */
     const handleSubmit = (e) => {
         e.preventDefault()
 
-        /* Validate the form inputs */
-        handleValidation()
-
-        /* Perform the login */
-        providers.authProvider.login(username, password)
-        .then((result) => {
-            setAccessToken(result)
-            /* redirect to the main page */
-            return navigate("/")
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+         
+            /* Perform the login */
+            providers.authProvider.login(username, password)
+            .then((result) => {
+                setAccessToken(result)
+                /* redirect to the main page */
+                return navigate("/")
+            })
+            .catch((err) => {
+                console.log(err)
+            })
 
     }
+
 
     /* render the component */
     return (
@@ -90,15 +64,13 @@ const LoginForm = () => {
             <h2 className="login-header">Login</h2>
             <label htmlFor="email" className="login-label">Email address:</label>
             <input type="text" id="email" className="login-input" placeholder="Email address" onChange={handleUsernameChange} />
+            {usernameErrors ? (<p key={nanoid()} className="formError">{usernameErrors}</p>) : null }
             <label htmlFor="password" className="login-label">Password:</label>
             <input type="password" id="password" className="login-input" onChange={handlePasswordChange} />
             <button className="btn login-button" type="submit">Submit</button>
-            {errors.map((error) => {
-                return (<p key={nanoid()} className="formError">{error}</p>)
-            })}
             <hr />
             <div aria-label="signup-container" className="flex flex-row signup-container">
-                No account? &nbsp; Then &nbsp; <Link to="/signup" className="">Signup</Link>
+                Need an account? &nbsp; Then &nbsp; <Link to="/signup" className="">Signup</Link>
             </div>
         </form>
     )
