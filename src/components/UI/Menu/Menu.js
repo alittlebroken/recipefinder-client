8b6fc6185a04c912200ac9a2baf071c7c13ad930
Paul Lockyer <plockyer@googlemail.com>
2023-09-03 22:02:17 +0100

    fleshed out code for dropdown menu

--- src/components/UI/Menu/Menu.js
+++ src/components/UI/Menu/Menu.js
@@ -1,16 +1,30 @@
 import './Menu.css'
 
+import { useState } from 'react'
+import { nanoid } from '@reduxjs/toolkit'
+import { Link } from 'react-router-dom'
+
 const Menu = (props) => {
 
     /* Destructure the passed in props */
     const { 
-        title
+        title,
+        items
     } = props
 
     return (
-        <>
-        
-        </>
+        <div aria-label="dropdown container" className="dropdown">
+            <a href="#" className="dropdown-title">
+                {title}
+            </a>
+            <div aria-label="dropdown content" className="dropdown-content">
+                {items.map(item => {
+                    return (
+                        <Link to={item.url} className="dropdown-link">{item.name}</Link>
+                    )
+                })}
+            </div>
+        </div>
     )
 
 }
