2a27021e4659444e950e15fc26f61a65e226e077
Paul Lockyer <plockyer@googlemail.com>
2023-09-25 08:36:55 +0100

    Initial creation of component and styles

---
+++ src/components/Client/Profile/Profile.js
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+import './Profile.css'
+
+import ProfileHero from './ProfileHero'
+import ProfileDataBar from './ProfileDataBar'
+import ProfilePersonalInfo from './ProfilePersonalInfo'
+import ProfilePassword from './ProfilePassword'
+
+const Profile = () => {
+
+    return (
+        <div aria-label="profile container" className="profile-container">
+
+            
+
+
+        </div>
+    )
+}
+
+export default Profile
