c806629d6f6e3309f76959ea928f0954c6b072ed
Paul Lockyer <plockyer@googlemail.com>
2023-10-02 16:47:17 +0100

    removed count badges from buttons

--- src/components/Client/Profile/ProfileDataBar.js
+++ src/components/Client/Profile/ProfileDataBar.js
@@ -13,10 +13,10 @@ const ProfileDataBar = (props) => {
 
     return (
         <div aria-label="profile data bar" className="pdb-container flex flex-row">
-          <Link to="/profile/pantry" data-descr="12" className="pdb-link-btn flex" >Pantry</Link>
-          <Link to="/profile/ingredients" data-descr="12" className="pdb-link-btn flex" >Ingredients</Link>
-          <Link to="/profile/recipes" data-descr="1" className="pdb-link-btn flex" >Recipes</Link>
-          <Link to="/profile/cookbooks" data-descr="1" className="pdb-link-btn flex" >Cookbooks</Link>
+          <Link to="/profile/pantry" className="pdb-link-btn flex" >Pantry</Link>
+          <Link to="/profile/ingredients" className="pdb-link-btn flex" >Ingredients</Link>
+          <Link to="/profile/recipes" className="pdb-link-btn flex" >Recipes</Link>
+          <Link to="/profile/cookbooks" className="pdb-link-btn flex" >Cookbooks</Link>
         </div>
     )
 
