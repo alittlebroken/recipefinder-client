477b6b70fdf91f357ad131865468c71d85000f1c
Paul Lockyer <plockyer@googlemail.com>
2023-10-04 16:20:31 +0100

    set to gather profile data from the store

--- src/components/Client/Profile/Profile.js
+++ src/components/Client/Profile/Profile.js
@@ -1,19 +1,60 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
 
 import './Profile.css'
 
-import ProfileHero from './ProfileHero'
+import ProfileHeader from './ProfileHeader'
 import ProfileDataBar from './ProfileDataBar'
 import ProfilePersonalInfo from './ProfilePersonalInfo'
-import ProfilePassword from './ProfilePassword'
+import ProfilePasswordReset from "./ProfilePasswordReset"
 
-const Profile = () => {
+/* Import the selectors and thunks for the profile component */
+import { 
+    getUserProfile, 
+    selectProfileData,
+    selectLoading,
+    selectHasError
+} from '../../../slices/Profile/Profile.slice'
+
+const Profile = (props) => {
+
+    /* Alias the despatch hook */
+    const dispatch = useDispatch()
+
+    let profileData = useSelector(selectProfileData)
+    let loading = useSelector(selectLoading)
+    let error = useSelector(selectHasError)
+
+    /* destructure the props */
+    const {
+        token
+    } = props
+
+    /* Load the users profile when the component is mounted */
+    useEffect(() => {
+        const fetchData = async () => {
+            console.log('During useEffect')
+            await dispatch(getUserProfile(token?.user?.id))
+        }
+        fetchData()
+    },[dispatch])
+
 
     return (
         <div aria-label="profile container" className="profile-container">
 
-            
-
+            { profileData && <ProfileHeader 
+                    src={profileData.avatar_src}
+                    alt={profileData.avatar_alt}
+                    title={profileData.avatar_title}
+                    forename={profileData?.forename}
+                    surname={profileData?.surname}
+                    email={profileData?.email}
+                />
+            }
+            { profileData && <ProfileDataBar /> }
+            { profileData && <ProfilePersonalInfo  user={profileData} /> }
+            { profileData && <ProfilePasswordReset /> }
 
         </div>
     )
