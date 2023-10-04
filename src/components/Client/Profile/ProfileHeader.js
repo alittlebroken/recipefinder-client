fc1021a88ad0fd568850c96f8a79df8134cec0cd
Paul Lockyer <plockyer@googlemail.com>
2023-10-04 12:38:27 +0100

    added src, title and alt props direct rather than in an object

--- src/components/Client/Profile/ProfileHeader.js
+++ src/components/Client/Profile/ProfileHeader.js
@@ -4,7 +4,9 @@ const ProfileHeader = (props) => {
 
     /* Destructure the passed in props */
     const {
-        avatar,
+        src,
+        alt,
+        title,
         forename,
         surname,
         email
@@ -12,7 +14,7 @@ const ProfileHeader = (props) => {
 
     return (
         <div aria-label="profile header container" className="ph-container flex flex-col flex-center">
-            <img src={avatar.url} alt={avatar.alt} title={avatar.title} className="ph-avatar"/>
+            <img src={src} alt={alt} title={title} className="ph-avatar"/>
             <h1 className="ph-fullname">{forename} {surname}</h1>
             <h3 className="ph-email">{email}</h3>
         </div>
