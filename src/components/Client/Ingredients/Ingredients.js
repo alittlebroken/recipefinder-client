1643631fd52cff4ddcde1757f7b89485f60d3532
Paul Lockyer <plockyer@googlemail.com>
2023-10-11 22:13:26 +0100

    added ability to add an ingredient to a users pantry

--- src/components/Client/Ingredients/Ingredients.js
+++ src/components/Client/Ingredients/Ingredients.js
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { nanoid } from "@reduxjs/toolkit"
 import Pagination from "../../UI/Pagination/Pagination"
+import authProvider from "../../../providers/authProvider"
+import apiProvider from "../../../providers/apiProvider"
+import Modal from "../../UI/Modal/Modal"
 
 import './Ingredients.css'
 
@@ -22,6 +25,7 @@ import {
     setRecsPerPage,
     goToPage
 } from '../../../slices/Ingredients/IngredientsSlice'
+import AbstractModalHeader from "react-bootstrap/esm/AbstractModalHeader"
 
 const Ingredients = () => {
 
@@ -44,6 +48,21 @@ const Ingredients = () => {
     /* State for controlling pagination */
     const [page, setPage] = useState(pagination.page)
     const [recsPage, setRecsPage] = useState(pagination.recsPerPage)
+
+    /* State for controlling any Modal elements */
+    const [showModal, setShowModal] = useState(false)
+    const [showAddIngredient, setShowAddIngredient] = useState(false)
+
+    /* State for the add Ingredient form */
+    const [ingredientName, setIngredientName] = useState()
+    const [ingredientId, setIngredientId] = useState()
+    const [amount, setAmount] = useState()
+    const [amountType, setAmountType] = useState()
+    const [amountError, setAmountError] = useState()
+    const [amountTypeError, setAmountTypeError] = useState()
+    const [addFormOk, setAddFormOk] = useState(true)
+    const [formUpdate, setFormUpdate] = useState()
+    const [formUpdateError, setFormUpdateError] = useState()
 
     /* What filter has been used */
     const filter = useSelector(selectFilter)
@@ -75,6 +94,57 @@ const Ingredients = () => {
 
     }
 
+    const handleSubmitAddIngredient = async (e) => {
+        e.preventDefault()
+
+        /* Perform validation on the inputs */
+        if(!parseInt(amount) || parseInt(amount) === undefined || typeof parseInt(amount) !== 'number' || parseInt(amount) < 1){
+            setAmountError('You must provide a valid number for amount that is greater than zero')
+            setAddFormOk(false)
+        }
+
+        if(!amountType || amountType === undefined || typeof amountType !== 'string' || amountType.length < 1){
+            setAmountTypeError('You must provide a valid amount type of at least 1 character')
+            setAddFormOk(false)
+        }
+
+        /* If the form has passed validation then lets try and add the data in the form */
+        if(addFormOk){
+
+            /* Get details from the auth API for the logged in user */
+            const { data } = await authProvider.getProfile()
+            
+            /* Generate the params to send along with the request to the API */
+            const params = {
+                auth: {
+                    authenticate: true,   
+                },
+                payload: {
+                    userId: data.id,
+                    pantryId: data.pantryId,
+                    ingredientId: e.target[0].value,
+                    amount: e.target[2].value,
+                    amount_type: e.target[3].value
+                }
+            }
+            
+            /* Send the request to the API  */
+            apiProvider.create('pantries', params)
+            .then((result) => {
+                if(result.status === 201){
+                    setFormUpdate('Ingredient successfully added to your pantry')
+                    return
+                }
+            })
+            .catch((error) => {
+                setFormUpdateError('Unable to add the ingredient to your pantry, please try again in a little while')
+                return
+            })
+
+        } 
+
+    }
+
     /* Handler for going forward or backward the pages */
     const pageChangeHandler = async (e) => {
         if(e.target.value === '-'){
@@ -94,6 +164,27 @@ const Ingredients = () => {
         dispatch(setRecsPerPage(e.target.value))
     }
 
+    /* Add an ingredient to our pantry */
+    const handleAddIngredient = async (e) => {
+        e.preventDefault()
+        
+        if(!authProvider.loggedIn()){
+            setShowModal(true);
+            return
+        }
+
+        /* users Id */
+        //const { id, pantryId } = await authProvider.getProfile().data
+
+        /* Show the Modal form for adding an Ingredient */
+        setShowAddIngredient(true)
+
+        /* Assign the various ingredient details for the add ingredient form */
+        setIngredientId(e.target.dataset.id)
+        setIngredientName(e.target.dataset.name)
+
+    }
+
     /* Load data when the component mounts */
     useEffect(() => {
         dispatch(getIngredients({
@@ -109,6 +200,70 @@ const Ingredients = () => {
     /* Render the compnent */
     return (
         <div aria-label="ig-contain" className="ig-contain flex flex-col">
+            
+            <Modal show={showModal} handleClose={() => {
+                setShowModal(false)
+                }}>
+                'You must be logged in to add an Ingredient to your pantry'
+            </Modal>
+
+            <Modal show={showAddIngredient} handleClose={() => {
+                setShowAddIngredient(false)
+                setAmountError(null)
+                setAmountTypeError(null)
+                setFormUpdate(null)
+                setFormUpdateError(null)
+            }}>
+
+                <form onSubmit={handleSubmitAddIngredient} className="ig-add-form flex flex-col">
+
+                    <h4 className="ig-add-heading">Add to pantry</h4>
+
+                    <label htmlFor="ingredient-name">Ingredient:</label>
+
+                    <input type="hidden" name="ingredient-id" value={ingredientId} />
+                    
+                    <input 
+                        type="text" 
+                        name="ingredient-name" 
+                        value={ingredientName} disabled />
+
+                    <label htmlFor="amount">Amount:</label>
+                    <input 
+                        type="number" 
+                        name="amount" 
+                        id="amount" 
+                        onChange={(e) => { 
+                            setAmount(e.target.value)
+                            setAmountError(null)
+                        }}
+                    />
+                    {amountError && (<p className="ig-add-error">{amountError}</p>)}
+                    
+                    <label htmlFor="amount-type">Amount Type:</label>
+                    <input 
+                        type="text" 
+                        name="amount-type" 
+                        onChange={(e) => { 
+                            setAmountType(e.target.value)
+                            setAmountTypeError(null) 
+                        }}
+                        value={amountType}
+                    />
+                    {amountTypeError && (<p className="ig-add-error">{amountTypeError}</p>)}
+
+                    <button 
+                        type="submit" 
+                        className="btn ig-add-btn">
+                            Add to Pantry
+                    </button>
+
+                    {formUpdate && (<p className="ig-add-ok">{formUpdate}</p>)}
+                    {formUpdateError && (<p className="ig-add-error">{formUpdateError}</p>)}
+
+                </form>
+            </Modal>
+
             <h2 className="ig-head-2">Ingredients</h2>
             <form id="filterIngredients" onSubmit={handleSubmit} className="flex flex-row ig-filter">
                 <input 
@@ -156,6 +311,16 @@ const Ingredients = () => {
                                     <div className="ig-container-text">
                                         {ingredient.name}
                                     </div>
+                                    <button 
+                                     className="btn ig-container-btn"
+                                     onClick={handleAddIngredient}
+                                     value={ingredient.id}
+                                     data-id={ingredient.id}
+                                     data-name={ingredient.name}
+                                     data-image={ingredient.src}
+                                     >
+                                        +
+                                    </button>
                                 </div>
                             )
                         })
