9fb98f2ad06ff7d3884ed7d33ea7c82355284ce0
Paul Lockyer <plockyer@googlemail.com>
2023-10-19 16:44:18 +0100

    Initial creation

---
+++ src/components/Client/Pantry/PantryFormEdit.js
@@ -0,0 +1,13 @@
+import { useState } from "react"
+import './PantryEditIngredient.css'
+
+const PantryFormEdit = (props) => {
+
+    return (
+        <form>
+
+        </form>
+    )
+} 
+
+export default PantryFormEdit
