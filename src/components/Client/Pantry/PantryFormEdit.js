57ad22fc24d7dce43d25798598e48210df626051
Paul Lockyer <plockyer@googlemail.com>
2023-10-20 14:12:17 +0100

    reset from inputs to controlled and set the correct payload

--- src/components/Client/Pantry/PantryFormEdit.js
+++ src/components/Client/Pantry/PantryFormEdit.js
@@ -6,28 +6,25 @@ const PantryFormEdit = (props) => {
 
     /* Destructure the props */
     const ingredient = JSON.parse(props.ingredient)
-    const closeModal = props.closeModal
+    const closeModal = props.modalShow
+    const setIsDirty = props.handleIsDirty
 
-    /* Hold state for the forms inputs */
-    const [amount, setAmount] = useState(ingredient.amount)
+    /* Hold state for the component */
+    const [amount, setAmount] = useState(ingredient?.amount || '')
+    const [amountType, setAmountType] = useState(ingredient.amountType || '')
     const [amountError, setAmountError] = useState()
-    const [amountType, setAmountType] = useState(ingredient.amountType)
     const [amountTypeError, setAmountTypeError] = useState()
     const [formOK, setFormOK] = useState(true)
 
-    /* handle the change for amount */
-    const handleAmount = (e) => {
-        setAmount(e.target.value)
-    }
-
-    /* handle the chnage for amountType */
-    const handleAmountType = (e) => {
-        setAmountType(e.target.value)
-    }
 
     /* Handle the submission of the form */
     const handleSubmit = async (e) => {
         e.preventDefault()
+
+        /* extarct out from the form the values we need */
+        const id = e.target[0].value
+        const ingredientId = e.target[1].value
+        const pantryId = e.target[2].value
 
         /* Validate the form fields */
         if(!amount || amount === undefined || parseInt(amount) < 1){
@@ -35,7 +32,7 @@ const PantryFormEdit = (props) => {
             setFormOK(false)
         }
 
-        if(!amountType || amountType === undefined || amountType === '' || amountType.length > 2){
+        if(!amountType || amountType === undefined || amountType === '' || amountType.length < 2){
             setAmountTypeError('You must supply a valid amount type of at least 2 characters')
             setFormOK(false)
         }
@@ -43,18 +40,30 @@ const PantryFormEdit = (props) => {
         if(formOK){
             /* Build the params to send to the API */
             const params = {
-                pantryId: parseInt(ingredient.pantry),
-                ingredientId: parseInt(ingredient.id),
-                amount,
-                amountType
+                auth: {
+                    authenticate: true
+                },
+                id: pantryId,
+                payload: {
+                    ingredients: [
+                        {
+                            id,
+                            ingredientId,
+                            pantryId,
+                            amount,
+                            amount_type: amountType
+                        }
+                    ]
+                }, 
             }
 
             /* Send the request to the API and check it succeded */
             const result = await apiProvider.update('pantries', params)
-
+            
             if(result.status === 200){
                 setAmountError(null)
                 setAmountTypeError(null)
+                setIsDirty(true)
                 closeModal(false)
             } 
         }
@@ -63,19 +72,24 @@ const PantryFormEdit = (props) => {
 
     return (
         <form onSubmit={handleSubmit} className="flex editForm">
+            <input type="hidden" name="id" value={ingredient.id} />
+            <input type="hidden" name="ingredientId" value={ingredient.ingredientId} />
+            <input type="hidden" name="pantry" value={ingredient.pantry} />
             <label htmlFor="amount" className="editFormLabel" >Amount:</label>
             <input 
                 type="number" 
                 name="amount" 
                 className="editFormInput" 
-                onChange={handleAmount}
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
             />
             <label htmlFor="amountType" className="editFormLabel" >Amount Type:</label>
             <input 
                 type="text" 
                 name="amountType" 
                 className="editFormInput" 
-                onChange={handleAmountType}
+                value={amountType}
+                onChange={(e) => setAmountType(e.target.value)}
             />
             <div aria-label="action button container" className="editFormActions">
                 <button type="submit" name="submit" className="btn editFormButton">Submit</button>
