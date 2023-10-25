5ca1aad198e3723fd3c33b49a2f7a4fac7881047
Paul Lockyer <plockyer@googlemail.com>
2023-10-25 20:59:43 +0100

    added html structure

--- src/components/Client/ProfileCookbooks/ProfileCookbooks.js
+++ src/components/Client/ProfileCookbooks/ProfileCookbooks.js
@@ -3,7 +3,26 @@ import './ProfileCookbooks.com'
 const ProfileCookbooks = (props) => {
 
     return(
-        <></>
+        <div aria-label="content container" className="flex flex-col">
+
+            <div aria-label="container for header and add button" className=" head-container flex">
+                <h2 className="pc-head-2">Add Cookbook</h2>
+                <button 
+                    type="submit" 
+                    value="new" 
+                    className="btn pc-btn-new"
+                >
+                    New
+                </button>
+            </div>
+
+            <div aria-label="cookbooks container" className="pc-cookbooks-container flex">
+
+                
+
+            </div>
+
+        </div>
     )
 }
 
