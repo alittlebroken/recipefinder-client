ae233770ffd42e2a8a084449425652ea71f06367
Paul Lockyer <plockyer@googlemail.com>
2023-10-26 07:54:49 +0100

    initial skeleton created

--- src/components/Client/ProfileCookbooks/CookbookCard.js
+++ src/components/Client/ProfileCookbooks/CookbookCard.js
@@ -1,5 +1,6 @@
 import './CookbookCard.css'
 
+
 const CookBookCard = (props) => {
 
     /* Destructure the passed in props */
@@ -8,9 +9,31 @@ const CookBookCard = (props) => {
     } = props
 
     return (
-        <>
-            
-        </>
+        <div aria-label="cookbook container" className="cc-container flex">
+
+            <img src={data.image_src} title={data.img_title} alt={data.img_alt} className="cc-image" />
+
+            <div aria-label="cookbook details" className="cc-details-container flex">
+
+                <h3 className="cc-head-3">{data.title}</h3>
+
+                <p className="cc-description">{data.description}</p>
+
+            </div>
+
+            <div aria-label="cookbook action button container" className="cc-action-buttons flex">
+
+                <button name="moreInfo" className="cc-btn-action-more" value="more">
+                    More Info
+                </button>
+
+                <button name="remove" className="cc-btn-action-remove" value="remove">
+                    Remove
+                </button>
+
+            </div>
+
+        </div>
     )
 
 }
