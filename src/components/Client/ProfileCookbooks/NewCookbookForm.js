cbd179e0cb8f0605bf3cedbdc7c733af8c10311e
Paul Lockyer <plockyer@googlemail.com>
2023-10-27 06:53:27 +0100

    added form elements to component

--- src/components/Client/ProfileCookbooks/NewCookbookForm.js
+++ src/components/Client/ProfileCookbooks/NewCookbookForm.js
@@ -1,9 +1,57 @@
 import './NewCookbookForm.css'
+import Form from '../../UI/Form/Form'
+import FormInput from '../../UI/Form/FormInput'
+import FormUpload from '../../UI/Form/FormUpload'
 
 const NewCookbookForm = (props) => {
 
+    /* Set the initial values for the form */
+    const initialValues = {
+        name: '',
+        description: '',
+        image: '',
+        title: '',
+        altText: ''
+    }
+
+    /* Handler for the form submission */
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+
     return (
-        <></>
+        <Form 
+            initialValues={initialValues}
+            submit={handleSubmit}
+        >
+            <FormInput 
+                name="name"
+                label="Name"
+            />
+
+            <FormInput 
+                name="description"
+                label="Description"
+            />
+
+            <FormUpload 
+                name="image"
+                label="Cookbook Image"
+                acceptType="image/*"
+            />
+
+            <FormInput 
+                name="title"
+                label="Image Title"
+            />
+
+            <FormInput 
+                name="altText"
+                label="Image Alternative Text"
+            />
+
+        </Form>
     )
 
 }
