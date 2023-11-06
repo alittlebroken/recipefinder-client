d5b6836c17dc96ceea91546c01c48727de9cfe76
Paul Lockyer <plockyer@googlemail.com>
2023-11-06 15:46:41 +0000

    added cookbook information from prop

--- src/components/Client/ProfileCookbook/ProfileCookbook.js
+++ src/components/Client/ProfileCookbook/ProfileCookbook.js
@@ -2,10 +2,32 @@ import './ProfileCookbook.css'
 
 const ProfileCookbook = (props) => {
 
+    /* Destructure the props passed in */
+    const {
+        cookbook
+    } = props
 
     return (
-        <>
-        </>
+        
+        <div aria-label="cookbook container" className="cb-container flex">
+
+            <img 
+                src={cookbook.src}
+                title={cookbook.title}
+                alt={cookbook.alt}
+            />
+
+            <h2 className="cb-head-2">
+                {cookbook.name}
+            </h2>
+
+            <div aria-label="cookbook description" className="cb-description">
+                {cookbook.description}
+            </div>
+
+
+        </div>
+
     )
 
 }
