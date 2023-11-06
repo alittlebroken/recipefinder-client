59d14644de8c59cf5acd7204ee020299de79c9b0
Paul Lockyer <plockyer@googlemail.com>
2023-11-06 16:26:16 +0000

    added recipe container and recipes

--- src/components/Client/ProfileCookbook/ProfileCookbook.js
+++ src/components/Client/ProfileCookbook/ProfileCookbook.js
@@ -25,6 +25,43 @@ const ProfileCookbook = (props) => {
                 {cookbook.description}
             </div>
 
+            <div aria-label="recipes container" className="cb-recipe-list flex">
+
+                {recipes.map( recipe => {
+
+                    <div aria-label="recipe-container" className="cb-recipe-container flex">
+
+                        <img 
+                            src={recipe.images[0].imageSrc} 
+                            title={recipe.images[0].imageTitle}
+                            alt={recipe.images[0].imageAlt}
+                        />
+
+                        <div aria-label="recipe content" className="cb-recipe-content flex">
+
+                            <div aria-label='recipe details' className="cb-recipe-details flex">
+
+                                <h3 className="cb-head-3">{recipe.name}</h3>
+
+                                <p aria-label="recipe description" className="cb-recipe-desc">
+                                    {recipe.description}
+                                </p>
+
+                            </div> 
+
+                            <div aria-label="recipe actions" className="cb-recipe-actions flex">
+
+                                <button name="remove" className="btn cb-action-btn flex">Remove</button>
+                                <button name="moreInfo" classname="btn cb-action-btn flex">More Info</button>
+                            </div> 
+
+                        </div>
+
+                    </div>
+
+                })}
+
+            </div>
 
         </div>
 
