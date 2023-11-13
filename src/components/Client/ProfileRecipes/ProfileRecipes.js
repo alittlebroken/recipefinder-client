95123e1032dc1a65a040f5dc0a0d349410a4f3d7
Paul Lockyer <plockyer@googlemail.com>
2023-11-13 13:06:05 +0000

    added base structure

--- src/components/Client/ProfileRecipes/ProfileRecipes.js
+++ src/components/Client/ProfileRecipes/ProfileRecipes.js
@@ -2,7 +2,26 @@ import './ProfileRecipes.css'
 
 const ProfileRecipes = () => {
 
-    return (<></>)
+    return (
+        <div aria-label="recipes container" className="pr-container flex">
+
+            <div aria-label="recipes header container" className="pr-header-container">
+                <h2 className="pr-head-2">Recipes</h2>
+                <button className="btn pr-btn-new">New</button>
+            </div>
+
+            <div aria-label="recipe filter container" className="pr-filter-container">
+
+
+            </div>
+
+            <div aria-label="recipes container" className="pr-recipes-container">
+
+            </div>
+
+
+        </div>
+    )
 }
 
 export default ProfileRecipes
