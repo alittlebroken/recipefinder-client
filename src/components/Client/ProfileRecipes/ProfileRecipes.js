b3d28862ea2e7e8d93a24d6c3c2881ddf279b219
Paul Lockyer <plockyer@googlemail.com>
2023-11-13 16:50:29 +0000

    added retrieval of data from slice

--- src/components/Client/ProfileRecipes/ProfileRecipes.js
+++ src/components/Client/ProfileRecipes/ProfileRecipes.js
@@ -1,6 +1,49 @@
 import './ProfileRecipes.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { 
+    fetchRecipes,
+    selectHasError,
+    selectIsLoading,
+    selectRecipes,
+    selectPage,
+    selectPages,
+    selectRecords,
+    selectRecsPerPage,
+} from '../../../slices/Recipes/Recipes.slice'
+
+import { 
+    selectProfileData,
+} from '../../../slices/Profile/Profile.slice'
 
 const ProfileRecipes = () => {
+
+    /* Alias the dispatcher */
+    const dispatch = useDispatch()
+
+    /* Gather the data from the store */
+    const recipes = useSelector(selectRecipes)
+
+    /* Get the users profile information */
+    const profile = useSelector(selectProfileData)
+
+    /* Load the data when the component is first mounted */
+    useEffect(() => {
+
+        /* Function to allow us to await the fetch of data from the 
+           API */
+        const fetchData = async () => {
+
+            /* Call the API to get us some records to look at */
+            await dispatch(fetchRecipes({ userId: profile.userId}))
+
+        }
+
+        /* Fetch the data */
+        fetchData()
+
+    }, [])
+
 
     return (
         <div aria-label="recipes container" className="pr-container flex">
