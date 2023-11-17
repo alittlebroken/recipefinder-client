21dfad4424c66c1be4953fcb97f3299846dccbab
Paul Lockyer <plockyer@googlemail.com>
2023-11-17 11:39:18 +0000

    initial creation of FormSelect

--- src/components/UI/Form/FormSelect.js
+++ src/components/UI/Form/FormSelect.js
@@ -6,6 +6,38 @@ import './FormSelect.css'
 
 const FormSelect = props => {
 
+    /* Extract the props */
+    const {
+        name,
+        label,
+        items,
+        optionDisplay,
+        optionValue,
+        defaultSelected
+    } = props
+
+    /* Get the form context to access the values needed */
+    const formContext = useContext(FormContext)
+    const { form, handleFormChange, setDirty } = formContext
+
+    return (
+        <div aria-label="select container" className="FormSelectContainer">
+
+            <label htmlFor={name} className="FormLabel">{label}:</label>
+            <select 
+                id={name}
+                name={name}
+                onChange={handleFormChange}
+            >
+                {items && items.map(item => {
+                    return (
+                        <option value={item[optionValue]}>{item[optionDisplay]}</option>
+                    )
+                })}
+            </select>
+        </div>
+    )
+
 }
 
 export default FormSelect
