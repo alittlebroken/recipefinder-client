134b83245ea8353a8c163fe1929dffb124088658
Paul Lockyer <plockyer@googlemail.com>
2023-11-22 08:37:08 +0000

    added ability to get more than one list at a time

--- src/hooks/useList.js
+++ src/hooks/useList.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
 import apiProvider from "../providers/apiProvider";
 
-export const useList = (resource) => {
+export const useList = (resource, multi = false) => {
 
     /* State to hold the list items we need */
-    const [list, setList] = useState(null)
+    const [list, setList] = useState()
 
     /* Here we get the data from the API and store it locally in out state */
     useEffect(() => {
@@ -22,9 +22,23 @@ export const useList = (resource) => {
                 }
             }
 
-            /* Make the call to the API */
-            const results = await apiProvider.getList(resource, params)
-            setList(results.data.results)
+            /* Check if we have more than one resource to load */
+            if(!multi){
+                /* get the only desired resource */
+                /* Make the call to the API */
+                let results = await apiProvider.getList(resource, params)
+                setList(results.data.results)
+            } else if (multi){
+                /* Loop through each resource passed in and extract the
+                 require data from the API.
+                 Pass it back as an array of objects */
+                resource.forEach( async data => {
+                    let resources = []
+                    let results = await apiProvider.getList(data.resource, params)
+                    resources.push({[data.name]: results.data.results})
+                    setList(resources)
+                })
+            }
 
         }
 
