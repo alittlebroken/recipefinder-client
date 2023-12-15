e70ceec4503f1063b307228a8766f781795e8ccf
Paul Lockyer <plockyer@googlemail.com>
2023-12-15 15:26:49 +0000

    refactored component layout

--- src/components/Client/Categories/CategoriesList.js
+++ src/components/Client/Categories/CategoriesList.js
@@ -3,12 +3,10 @@ import { nanoid } from '@reduxjs/toolkit'
 import { Link } from 'react-router-dom'
 
 const CategoriesList = ({ data }) => {
-    
-    console.log(data)
 
     if(!data || data?.length < 1){
         return (
-            <div aria-label="container for no records" className="noRecords">
+            <div aria-label="container for no records" className="noRecords flex">
                 No records found
             </div>
         )
@@ -17,8 +15,17 @@ const CategoriesList = ({ data }) => {
             <div aria-label="List of categories" className="categoriesList flex">
                 {data && data.map( record => {
                     return (
-                        <div key={nanoid} aria-label="category container" className="categoryContainer">
-                            <Link to={`/recipes/${record.name.toLowerCase()}`}>{record.name}</Link>
+                        <div 
+                            key={nanoid()} 
+                            aria-label="category container" 
+                            className="categoryContainer"
+                        >
+                            <Link 
+                                to={`/recipes/${record.name.toLowerCase()}`}
+                                className="categoryLink"
+                            >
+                                {record.name}
+                            </Link>
                         </div>
                     )
                 })}
