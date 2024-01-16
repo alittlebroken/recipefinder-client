692c9f34d01092974bc71f27e2d7f6702d17d940
Paul Lockyer <plockyer@googlemail.com>
2024-01-16 14:19:32 +0000

    can speciy size of modal via props

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,9 +1,29 @@
 import './Modal.css'
 
-const Modal = ({handleClose, show, children}) => {
+const Modal = ({
+    handleClose, 
+    show, 
+    children, 
+    sz600 = undefined,
+    sz900 = undefined,
+    sz1024 = undefined,
+    sz90p = undefined,
+    sz75p = undefined,
+    sz50p = undefined,
+    classes = undefined
+}) => {
 
     /* set the classes to apply if the Modal is open or not */
-    const showHideClass  = show ? "modal modal-display" : "modal modal-none"
+    let showHideClass  = show ? "modal modal-display" : "modal modal-none"
+
+    /* add additional classes if needed */
+    showHideClass += sz600 ? ' modal-sz600' : ''
+    showHideClass += sz900 ? ' modal-sz900' : ''
+    showHideClass += sz1024 ? ' modal-sz1024' : ''
+    showHideClass += sz50p ? ' modal-sz50p' : ''
+    showHideClass += sz75p ? ' modal-sz75p' : ''
+    showHideClass += sz90p ? ' modal-sz90p' : ''
+    showHideClass += classes ? classes : ''
 
     return (
         <div aria-label="modal container" className={showHideClass}>
