48c88d0cf1d45b283a66632ea94605fd088709da
Paul Lockyer <plockyer@googlemail.com>
2024-01-16 17:05:34 +0000

    added ability to apply new styles via props

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,30 +4,32 @@ const Modal = ({
     handleClose, 
     show, 
     children, 
-    sz600 = undefined,
-    sz900 = undefined,
-    sz1024 = undefined,
-    sz90p = undefined,
-    sz75p = undefined,
-    sz50p = undefined,
-    classes = undefined
+    sz600,
+    sz900,
+    sz1024,
+    sz90p,
+    sz75p,
+    sz50p,
+    classes,
+    iStyles
 }) => {
 
     /* set the classes to apply if the Modal is open or not */
     let showHideClass  = show ? "modal modal-display" : "modal modal-none"
 
     /* add additional classes if needed */
-    showHideClass += sz600 ? ' modal-sz600' : ''
-    showHideClass += sz900 ? ' modal-sz900' : ''
-    showHideClass += sz1024 ? ' modal-sz1024' : ''
-    showHideClass += sz50p ? ' modal-sz50p' : ''
-    showHideClass += sz75p ? ' modal-sz75p' : ''
-    showHideClass += sz90p ? ' modal-sz90p' : ''
-    showHideClass += classes ? classes : ''
+    let mainClasses = 'modal-main default-size'
+    mainClasses += sz600 ? ' modal-sz600' : ''
+    mainClasses += sz900 ? ' modal-sz900' : ''
+    mainClasses += sz1024 ? ' modalsz1024' : ''
+    mainClasses += sz50p ? ' modal-sz50p' : ''
+    mainClasses += sz75p ? ' modal-sz75p' : ''
+    mainClasses += sz90p ? ' modal-sz90p' : ''
+    mainClasses += classes ? classes : ''
 
     return (
         <div aria-label="modal container" className={showHideClass}>
-            <div aria-label="modal content" className="modal-main">
+            <div aria-label="modal content" className={mainClasses} style={iStyles}>
                 {children}
                 <button className="modal-close" type="button" onClick={handleClose}>X</button>
             </div>
