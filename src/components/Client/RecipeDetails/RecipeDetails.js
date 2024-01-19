86e767b52f2072cc9f91853c5a725d83a75b02ff
Paul Lockyer <plockyer@googlemail.com>
2024-01-19 11:38:25 +0000

    added styles and code to add a recipe to a cookbook

--- src/components/Client/RecipeDetails/RecipeDetails.js
+++ src/components/Client/RecipeDetails/RecipeDetails.js
@@ -2,6 +2,7 @@ import './RecipeDetails.css'
 
 import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 import apiProvider from '../../../providers/apiProvider'
 
 import Carousel from '../../UI/Carousel/Carousel'
@@ -12,6 +13,12 @@ import RecipeCategories from './RecipeCategories'
 
 import { useRecipe } from '../../../hooks/useRecipe'
 
+import { 
+    selectProfileData,
+} from '../../../slices/Profile/Profile.slice'
+import Modal from '../../UI/Modal/Modal'
+import { useCookbooks } from '../../../hooks/useCookbooks'
+
 const RecipeDetails = () => {
 
     /* Extract the details for the recipe */
@@ -21,6 +28,26 @@ const RecipeDetails = () => {
     const [isLoading, setIsLoading] = useState(true)
     const [errors, setErrors] = useState(null)
     const [recipe, setRecipe] = useState(null)
+
+    /* Get the user profile data */
+    let profile = useSelector(selectProfileData)
+
+    /* Gather a list of the users cookbooks
+       but only if we are logged in
+    */
+    const cookbooks = useCookbooks('cookbooks', profile.userId)
+
+    /* State for working on the current record the user has selected, for removal etc */
+    const [currentRecipe, setCurrentRecipe] = useState(null)
+
+    /* State for controlling the modals */
+    const [showModalAdd, setShowModalAdd] = useState(false)
+
+    /* State for which cookbook we need to add the recipe to */
+    const [selectedCookbook, setSelectedCookbook] = useState()
+
+    /* Set state for notifications */
+    const [notifications, setNotifications] = useState()
 
     /* Load the recipe details */
     useEffect(() => {
@@ -48,8 +75,6 @@ const RecipeDetails = () => {
 
     },[])
 
-    console.log(recipe)
-
     /* Generate the slides for the carousel */
     let carouselSlides = []
     recipe?.images && recipe?.images?.forEach(image => {
@@ -75,6 +100,81 @@ const RecipeDetails = () => {
     return(
         <div aria-label="recipe detail container" className="rd-recipeContainer flex">
 
+            <Modal
+                show={showModalAdd}
+                handleClose={(e) => {
+                    e.preventDefault()
+                    setShowModalAdd(false)
+                }}
+            >
+                <h3 className="selectModalHeading">Add recipe to cookbook?</h3>
+                <div aria-label="" className="selectContainer flex">
+                    <select 
+                        name="cookbook" 
+                        id="cookbook" 
+                        className="cookbookSelect"
+                        onChange={(e) => {
+                            e.preventDefault()
+                            setSelectedCookbook(e.target.value)
+                        }}
+                        defaultValue="Please select a cookbook"
+                    >
+                        <option disabled value={null}>Please select a cookbook</option>
+                        {cookbooks && cookbooks.map(cookbook => {
+                            return (
+                                <option value={cookbook.id}>{cookbook.name}</option>
+                            )
+                        })}
+                    </select>
+                    <button 
+                        className="btn selectBtn"
+                        value={{
+                            cookbookId: selectedCookbook,
+                            recipeId: currentRecipe
+                        }}
+                        onClick={async (e) => {
+                            e.preventDefault()
+                            
+                            /* Create the params to send to the API */
+                            const params = {
+                                id: parseInt(selectedCookbook),
+                                auth: {
+                                    authenticate: true
+                                },
+                                payload: {
+                                    recipeId: currentRecipe
+                                }
+                            }
+
+                            /* Send the request off */
+                            const res = await apiProvider.create("cookbookRecipes",params)
+                            
+                            if(res.status >= 200 && res.status < 300){
+                               console.log("recipe added to cookbook")
+                               setNotifications({
+                                className: 'notif-ok',
+                                message: 'Recipe successfully added to cookbook.'
+                                })
+                               setCurrentRecipe(null)
+                               setSelectedCookbook(null)
+                               setShowModalAdd(false) 
+                            } else {
+                               setNotifications({
+                                className: "notif-error",
+                                message: 'Unable to add recipe to cookbook.'
+                                })
+                               setCurrentRecipe(null)
+                               setSelectedCookbook(null)
+                               setShowModalAdd(false) 
+                            }
+
+                        }}
+                    >
+                        Add
+                    </button>
+                </div>
+            </Modal>
+
             <div 
                 aria-label="recipe header container" 
                 className="rd-recipeHeaderContainer"
@@ -82,6 +182,17 @@ const RecipeDetails = () => {
             >
                 <h2 className="rd-recipeTitle">{recipe?.name || null}</h2>
             </div>
+
+            {profile.userId && (
+                <div aria-label="container for add recipe to cookbook button" className="rd-cookbook-add-container flex">
+                    <button
+                     className="btn rd-add-to-cookbook"
+                     onClick={(e) => {
+                        setCurrentRecipe(recipe.id)
+                        setShowModalAdd(true)
+                    }}>Add to cookbook</button>
+                </div>
+            )}
 
             <RecipeCategories categories={recipe?.categories} />
 
@@ -123,6 +234,12 @@ const RecipeDetails = () => {
                 </div>
 
             </div>
+            
+            {notifications && (
+                <div aria-label="notifications container" className={notifications.className}>
+                    {notifications.message}
+                </div>
+            )}
 
         </div>
     )
